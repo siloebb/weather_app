714153bac146ad4f0d206a6c518991b630b855f4
siloebezerrabispo <siloebb@gmail.com>
2024-08-02 16:54:41 -0300

    feat: call ios native

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,16 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+      let channel = FlutterMethodChannel(name: "com.example.app/weather_app", binaryMessenger: controller.binaryMessenger)
+
+      channel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+        if call.method == "getPlatformVersion" {
+          result("iOS " + UIDevice.current.systemVersion)
+        } else {
+          result(FlutterMethodNotImplemented)
+        }
+      }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
